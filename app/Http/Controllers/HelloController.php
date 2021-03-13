2713ca897391097e1431df1cc2c4c46a6450e6ae
NagahamaTakahiro <nnarannnara27@gmail.com>
2021-03-13 10:57:28 +0900

    first-commit

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    public function index()
+    {
+        return "建物です";
+    }
+    
+    public function end()
+    {
+        return "部屋番号は{room}です";
+    }
+}
